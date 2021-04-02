86bb16d3444499af066dd589bbf63c8198c71895
Vetu Daniel <vetu.daniel@gmail.com>
2021-04-02 12:59:21 +0200

    v.1.1

--- app.js
+++ app.js
@@ -10,19 +10,19 @@ function init(){
 
     scene = new THREE.Scene();
 
-    const fov = 35;
+    const fov = 5;
     const aspect = container.clientWidth / container.clientHeight;
-    const near = 0.1;
+    const near = 10;
     const far = 800;
 
     camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.set(0, 0, 680);
+    camera.position.set(0, 0,50);
 
     const ambient = new THREE.AmbientLight(0x404040,2);
     scene.add(ambient);
 
-    const light = new THREE.DirectionalLight(0xffffff, 2.5);
-    light.position.set(50, 25, 100);
+    const light = new THREE.DirectionalLight(0xffffff, 1.8);
+    light.position.set(1000, 1000,100);
     scene.add(light)
 
     renderer = new THREE.WebGLRenderer({antialias:true, alpha:true});
@@ -54,3 +54,29 @@ function onWindowResize(){
 }
 
 window.addEventListener('resize', onWindowResize)
+
+
+function stars(){
+    let count = 230;
+    let scene = document.querySelector('body')
+    let i = 0;
+        while( i < count){
+            let star = document.createElement('i');
+            let x = Math.floor(Math.random()* window.innerWidth)
+            let y = Math.floor(Math.random()* window.innerHeight)
+            let duration = Math.random()* 10
+            let size = Math.random()* 2
+
+            star.style.left = x + 'px'
+            star.style.top = y + 'px'
+            star.style.width = 1 + size +  'px'
+            star.style.height = 1 + size +  'px'
+
+            star.style.animationDuration = 2 + duration +  's'
+            star.style.animationDelay = 1 + duration +  's'
+
+            scene.appendChild(star)
+            i++
+        }
+}
+stars()
