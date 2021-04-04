452fe9b01882c92f34f0e60929e3c387e0be0964
Vetu Daniel <vetu.daniel@gmail.com>
2021-04-04 13:46:47 +0200

    v1.5

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 const navContainer = document.querySelector('.nav-container');
 const hamburger = document.querySelector('.hamburger')
 const bars = document.querySelectorAll('.bar');
+const closeHamburgerMenu = document.querySelector('.close-icon');
+const overlay = document.querySelector('.overlay')
+
+
 
 
 hamburger.addEventListener('click', () => {
     navContainer.classList.toggle('active')
-    
-    bars.forEach(bar => {
-        bar.classList.toggle('toggle') 
-    })
+    hamburger.style.display = 'none'
+    overlay.classList.toggle('active')
+})
+
+closeHamburgerMenu.addEventListener('click',  () =>{
+    navContainer.classList.toggle('active') 
+    hamburger.style.display = 'block'
+    overlay.classList.toggle('active')
 })
